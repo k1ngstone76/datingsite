fc7f922b2782b45a2d770cdb29ba29a704eb9299
mrlis <mrlis1231.groot@gmail.com>
2024-04-24 23:35:56 +0300

    короче если ошибка из за remember me сносите его в ноль щяс робит регитстрация и +- вход

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def login():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)  # тыц, там session
             return redirect("/")
         return render_template('login.html', message="Неправильный логин или пароль", form=form)
     return render_template('login.html', title='Авторизация', form=form)
